03151d96d8d44743ae7c1f24dd88fa77ce798c05
Denis Agureev <dkihada@gmail.com>
2021-09-02 20:07:18 +0500

    create goodsList.jsx

---
+++ src/components/main/goodsList/goodsList.jsx
@@ -0,0 +1,19 @@
+import { GoodsItem } from './goodsItem/goodsItem';
+
+function GoodsList(props) {
+  const { goods = [] } = props;
+
+  if (!goods.length) {
+    return <h3>Ничего не найдено</h3>;
+  }
+
+  return (
+    <div className='columns is-flex-wrap-wrap mt-2 is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd'>
+      {goods.map((item) => {
+        return <GoodsItem key={item.mainId} {...item} />;
+      })}
+    </div>
+  );
+}
+
+export { GoodsList };
