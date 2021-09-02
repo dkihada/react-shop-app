86d7d6fae1c60460e783ce88817f0ddfc052b43f
Denis Agureev <dkihada@gmail.com>
2021-09-02 20:07:33 +0500

    create goodsItem.jsx

---
+++ src/components/main/goodsList/goodsItem/goodsItem.jsx
@@ -0,0 +1,42 @@
+function GoodsItem(props) {
+  const { mainId, displayName, displayDescription, price, displayAssets } =
+    props;
+
+  const imgBackgorund = displayAssets.map((item) => item.full_background);
+
+  return (
+    <div className='column is-one-quarter'>
+      <div
+        id={mainId}
+        className='card is-flex is-flex-direction-column'
+        style={{ height: '100%' }}
+      >
+        <div className='card-image'>
+          <figure className='image is-4by3'>
+            <img className='mt-3' src={imgBackgorund} alt={displayName} />
+          </figure>
+        </div>
+
+        <div className='px-5 pt-5 is-flex-grow-1'>
+          <div className='media'>
+            <div className='media-content'>
+              <p className='title is-4'>{displayName}</p>
+            </div>
+          </div>
+
+          <div>{displayDescription}</div>
+        </div>
+        <div className='card-content'>
+          <div className='is-flex is-justify-content-space-between is-align-items-center mt-5'>
+            <button className='button is-rounded'>Купить</button>
+            <span className='is-size-4 has-text-weight-medium '>
+              {price.regularPrice} $
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { GoodsItem };
