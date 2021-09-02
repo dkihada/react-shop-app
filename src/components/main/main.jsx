5d4e486c4cf35567dd75e7c27201871305a92e47
Denis Agureev <dkihada@gmail.com>
2021-09-02 20:09:41 +0500

    add useState and useEffect in main

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,7 +1,34 @@
-import "./main.css";
+import React, { useEffect, useState } from 'react';
+import { API_KEY, API_URL } from '../../config';
+import { Basket } from '../basket/basket';
+import { Preloader } from '../preloader/preloader';
+import { GoodsList } from './goodsList/goodsList';
+import './main.css';
 
 function Main() {
-  return <main className='container content'></main>;
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        data.shop && setGoods(data.shop);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <main className='container content'>
+      <Basket quantity={order.length} />
+      {loading ? <Preloader /> : <GoodsList goods={goods} />}
+    </main>
+  );
 }
 
 export { Main };
