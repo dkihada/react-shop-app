a4846a9e941261b3396532fca00f150284ede832
Denis Agureev <dkihada@gmail.com>
2021-09-19 16:06:41 +0500

    create reducer.js

---
+++ src/reducer.js
@@ -0,0 +1,80 @@
+export function reducer(state, { type, payload }) {
+  switch (type) {
+    case 'REMOVE_FROM_BASKET':
+      return {
+        ...state,
+        order: state.order.filter((el) => el.mainId !== payload.mainId),
+      };
+    case 'ADD_TO_BASKET': {
+      const itemIndex = state.order.findIndex(
+        (orderItem) => orderItem.mainId === payload.mainId
+      );
+      let newOrder = null;
+      if (itemIndex < 0) {
+        const newItem = {
+          ...payload,
+          quantity: 1,
+        };
+        newOrder = [...state.order, newItem];
+      } else {
+        newOrder = state.order.map((orderItem, index) => {
+          if (index === itemIndex) {
+            return {
+              ...orderItem,
+              quantity: orderItem.quantity + 1,
+            };
+          } else {
+            return orderItem;
+          }
+        });
+      }
+      return {
+        ...state,
+        order: newOrder,
+        alertName: payload.displayName,
+      };
+    }
+    case 'ADD_ITEM_TO_BASKET':
+      return {
+        ...state,
+        order: state.order.map((item) => {
+          if (item.mainId === payload.mainId) {
+            const newQuantity = item.quantity + 1;
+            return {
+              ...item,
+              quantity: newQuantity,
+            };
+          } else {
+            return item;
+          }
+        }),
+      };
+    case 'REMOVE_ITEM_TO_BASKET':
+      return {
+        ...state,
+        order: state.order.map((item) => {
+          if (item.mainId === payload.mainId) {
+            const newQuantity = item.quantity - 1;
+            return {
+              ...item,
+              quantity: newQuantity >= 0 ? newQuantity : 0,
+            };
+          } else {
+            return item;
+          }
+        }),
+      };
+    case 'HANDLE_BASKET_SHOW':
+      return {
+        ...state,
+        isBasketShow: !state.isBasketShow,
+      };
+    case 'ClOSE_ALERT':
+      return {
+        ...state,
+        alertName: '',
+      };
+    default:
+      return state;
+  }
+}
